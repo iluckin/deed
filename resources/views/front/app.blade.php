69405aa8cdefc522603b9eaef9ce818d747eede6
enhe <info@wowphp.cn>
2019-07-25 17:40:45 +0800

    update

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -1,32 +1,27 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title') 有人有事 @show</title>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>圆堂科技</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta content="width=device-width, initial-scale=.5, maximum-scale=.5, user-scalable=0;" name="viewport" />
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/h5/app.css') }}">
 </head>
-<style>
-    .element::-webkit-scrollbar { width: 0 !important }
-    .element { -ms-overflow-style: none; }
-    .element { overflow: -moz-scrollbars-none; }
-    * {
-        scroll-behavior: unset;
-    }
-</style>
-<body style="min-height: 10px;">
-<div id="app">
-    <div class="container-fluid">
-        @yield('content')
-    </div>
-</div>
+<body>
+<main id="app">
+    @yield('content')
+</main>
 </body>
+<script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.bootcss.com/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!--<script src="https://unpkg.com/feather-icons"></script>-->
+<script src="https://cdn.bootcss.com/feather-icons/4.22.1/feather.min.js"></script>
+<script src="{{ asset('js/h5.js') }}"></script>
+<script>
+    feather.replace({
+        width: 42,
+        height: 42
+    })
+</script>
 </html>
--- resources/views/front/services/index.blade.php
+++ resources/views/front/services/index.blade.php
@@ -1,76 +1,100 @@
+
 @extends('front.app')
+
 @section('content')
-    <section class="user-area mt-2">
-        <div class="row">
-            <div class="col-4 pl-1">
-                <a class="mt-1 px-0 d-inline-block nav-link font-weight-bold text-dark title" href="javascript:void 0">
-                    有人有事
-                </a>
-            </div>
-            <div class="col-8 pr-0">
-                <div class="row pull-right">
-                    <div class="ml-0 col-auto d-flex justify-content-end text-right px-0">
-                        <a class="mt-2 d-inline-block nav-link text-dark" href="#">
-                            登陆享更优质服务<i class="fa fa-arrow-right"></i>
-                        </a>
-                    </div>
-                    <div class="col-1 pl-0">
-                        <img class="service-avatar rounded rounded-circle border-light border-1" src="{{ asset('images/avatar.png') }}" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+<div class="container-fluid">
     <section class="banner">
-        <div class="row">
-            <div class="col px-0 mt-1">
-                <div id="carouselExampleIndicators" class="carousel slide banner-box shadow rounded" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="rounded-pill active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1" class="rounded-pill"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        <div class="rounded-pill carousel-item active" data-interval="2500">
-                            <img src="{{ asset('images/banners/item-2.png') }}" class="d-block w-100 rounded" alt="">
-                        </div>
-                        <div class="rounded-pill carousel-item" data-interval="2500">
-                            <img src="{{ asset('images/banners/item-3.png') }}" class="d-block w-100 rounded" alt="">
-                        </div>
+        <div class="col mt-4">
+            <div id="carouselExampleIndicators" class="carousel slide shadow" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100 banner-item" src="http://deed.static.wowphp.cn/640.jpeg" alt="First slide">
+                        <!--								<div class="carousel-caption d-none d-md-block">-->
+                        <!--									<h4 class="display-5 bold">圆堂科技产权办理产权服务上线了</h4>-->
+                        <!--								</div>-->
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 banner-item" src="http://deed.static.wowphp.cn/images/banners/20190725/B5d390ff8daac9.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 banner-item" src="http://deed.static.wowphp.cn/images/banners/20190725/B5d390ff8daac9.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 banner-item" src="http://deed.static.wowphp.cn/images/banners/20190725/B5d390ff8daac9.jpg" alt="First slide">
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <section class="services">
-        <div class="row mt-2">
-            <div class="col-4 px-0 shadow-sm">
-                <a href="{{ route('service.someone.deed') }}">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                <i class="fa fa-fort-awesome text-dark"></i>
-                            </h5>
-                            <a href="#" class="text-dark btn-link card-link">产权查询</a>
-                        </div>
-                    </div>
+    <section class="notice mt-3">
+        <div class="row align-items-center">
+            <div class="col pl-4 align-items-center pr-0 mr-0">
+                <i class="notice-icon text-warning bold" data-feather="volume-1"></i>
+            </div>
+            <div class="col-9 pt-3 pl-0 ml-0">
+                <a href="" class="text-muted">
+                    <p class="display-5">💐 圆堂产权办理查询服务上线啦～</p>
                 </a>
             </div>
-            <div class="col-4 px-0 mx-2 shadow-sm">
-                <div class="card text-center">
-                    <div class="card-body">
-                        <h5 class="card-title"><i class="fa fa-yelp text-dark"></i></h5>
-                        <a href="https://dache.meituan.com/ent/dache/home?channelId=203&qcs_channel=121003">测试链接</a>
+            <div class="col">
+                <a href="#">
+                    <i class="notice-icon text-muted" data-feather="more-horizontal"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+    <section class="title mt-3 mx-2">
+        <div class="row">
+            <div class="col">
+                <p>
+                    <span class="display-4">产权服务 </span>
+                    <span class="text-muted"> 专业服务、品质生活</span>
+                </p>
+            </div>
+        </div>
+    </section>
+    <section class="service">
+        <div class="row mx-2 align-items-center">
+            <div class="col mx-0 px-0">
+                <div class="card">
+                    <div class="card-body justify-content-center">
+                        <p class="card-text">
+                            <img style="height: 60px;" src="{{ asset('images/h5/house.png') }}" alt="">
+                        </p>
+                        <h5 class="card-title">商业产权查询</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">省心，放心</h6>
                     </div>
                 </div>
             </div>
-            <div class="col-4 px-0 mx-2 shadow-sm" onclick="alert('更多服务敬请期待！')">
-                <div class="card text-center">
-                    <div class="card-body">
-                        <h5 class="card-title"><i class="fa fa-yelp text-dark"></i></h5>
-                        敬请期待
+            <div class="col mx-0 px-2">
+                <div class="card">
+                    <div class="card-body justify-content-center">
+                        <p class="card-text">
+                            <img style="height: 60px;" src="{{ asset('images/h5/house.png') }}" alt="">
+                        </p>
+                        <h5 class="card-title">商业产权查询</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">省心，放心</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col mx-0 px-0">
+                <div class="card">
+                    <div class="card-body justify-content-center">
+                        <p class="card-text">
+                            <img style="height: 60px;" src="{{ asset('images/h5/house.png') }}" alt="">
+                        </p>
+                        <h5 class="card-title">商业产权查询</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">省心，放心</h6>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+</div>
 @endsection
